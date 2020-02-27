67befa919abfccc45f98f446d1a089dafe0c275e
Badger42S <k3gbdia1@gmail.com>
2020-02-27 18:47:09 -0400

    layer is changed

--- src/components/Layers/Layer.js
+++ src/components/Layers/Layer.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{ useState} from 'react';
 import Aux from '../../hoc/Auxilary';
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,35 +6,29 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import {connect} from 'react-redux';
 
-class Layer extends Component{
-    state={
-        showSideDrawer: false,
+const Layer =props=>{
+    const [showSideDrawer, setShowSideDrawer]=useState(false);
+    const sideDrawerCloseHandler=()=>{
+        setShowSideDrawer(false);
     }
-    sideDrawerCloseHandler=()=>{
-        this.setState({showSideDrawer:false});
+    const toggleHandler=()=>{
+        setShowSideDrawer(!showSideDrawer);
     }
-    toggleHandler=()=>{
-        this.setState(prevState=>{
-            return({showSideDrawer:!prevState.showSideDrawer});
-        });
-    }
-    render(){
         return(
                 <Aux>
                     <Toolbar
-                        isAuth={this.props.isAuth}
-                        menuShow={this.toggleHandler}
+                        isAuth={props.isAuth}
+                        menuShow={toggleHandler}
                     />
                      <SideDrawer 
-                     isAuth={this.props.isAuth}
-                     open={this.state.showSideDrawer} 
-                     closed={this.sideDrawerCloseHandler}/>
+                     isAuth={props.isAuth}
+                     open={showSideDrawer} 
+                     closed={sideDrawerCloseHandler}/>
                     <main className={styles.Content}>
-                     {this.props.children}
+                     {props.children}
                      </main>
                 </Aux>);
     };
-  }
   
  const mapStateToProps =state=>{
      return {
